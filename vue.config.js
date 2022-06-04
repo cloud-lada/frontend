5a9bee4f51a6ff2f8e8e5fd6e0818dd7e6cc5c9e
David Bond <davidsbond@users.noreply.github.com>
2022-06-04 17:48:04 +0100

    Connect live statistics to API (#6)
    This commit connects the LiveStatistics component to the API call that returns
    the latest statistics. The component will then rerequest data every 10 seconds.
    
    Signed-off-by: David Bond <davidsbond93@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
+  devServer: {
+    proxy: "https://lada.dsb.dev",
+  },
 });
