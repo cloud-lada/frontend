7876df8d0b4c560264e8efb7a1a3547ffddd5788
David Bond <davidsbond93@gmail.com>
2022-06-08 18:00:44 +0100

    Add daily charts
    This commit adds the StatisticChart component that can dynamically display the
    day's worth of data for a particular sensor.
    
    Signed-off-by: David Bond <davidsbond93@gmail.com>

---
+++ src/lib/reading/index.ts
@@ -0,0 +1,11 @@
+/**
+ * The SensorType enum contains all valid sensor types.
+ */
+export enum SensorType {
+  Speed = "speed",
+  Fuel = "fuel",
+  EngineTemperature = "engine_temperature",
+  Revolutions = "revolution",
+  LocationLatitude = "location_latitude",
+  LocationLongitude = "location_longitude",
+}
